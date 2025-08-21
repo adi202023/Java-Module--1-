64c85d4c726cceab02a1da12e196df48029d5deb
adi202023 <adityasheregar2006@gmail.com>
2025-08-21 11:58:28 +0530

    Greater.java

--- Greater.java
+++ Greater.java
@@ -3,18 +3,14 @@ import java.util.Scanner;
 public class Greater {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter three numbers:");
+        System.out.print("Enter two numbers:");
         int a = input.nextInt();
         int b = input.nextInt();
-        int c = input.nextInt();
-        if(a>=b && a>=c){
-            System.out.println(a +"is greatest number:");
+        if(a>b){
+            System.out.println(a +" is greatest number:");
         }
-        else if(b>=a && b>=c){
-            System.out.println(b +"is greatest number");
-        }
-        else {
-            System.out.println(c +"is greatest number");
+        else{
+            System.out.println(b +" is greatest number");
         }
     }
 }
